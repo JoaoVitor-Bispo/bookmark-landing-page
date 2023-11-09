fe0fee2ca7e8a65592906d4c6f1d3b6b2dce153f
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-11-09 17:27:22 -0300

    design mobile quase pronto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main-image': '../images/'
+      }
+    },
   },
   plugins: [],
 }
